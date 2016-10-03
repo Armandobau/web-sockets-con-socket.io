95bb5bdf9805f933c5536bfdb0868a6726b20998
Armando Bautista <armandoloja92@gmail.com>
2016-10-03 19:45:10 +0200

    upload

---
+++ server.js
@@ -0,0 +1,26 @@
+//en cmd, en la carpeta del proyecto: node server.js
+//creara un servidor node para poder funcionar
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+    
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
